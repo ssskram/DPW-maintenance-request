52d2c2f71148121ab03fe1bdacf7cd35a4a07091
ssskram <paul.marks@pittsburghpa.gov>
2018-03-01 13:57:05 -0500

    first commit

---
+++ Controllers/Home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DPW_maintenancerequest.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DPW_maintenancerequest.Controllers
+{
+    [Authorize]
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// auto expand textarea height to fit contents
+function textAreaAdjust(o) {
+    o.style.height = "1px";
+    o.style.height = (25+o.scrollHeight)+"px";
+    }
+
+// show overlay spinner on network activity
+// just add class "overlaytrigger" to buttons for this to pop off
+var classname = document.getElementsByClassName('overlaytrigger');
+var overlay = function() {
+    document.getElementById('overlayloader').style.display = 'flex';
+};
+Array.from(classname).forEach(function(element) {
+    element.addEventListener('click', overlay);
+  });
