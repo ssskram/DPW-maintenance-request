139aa77d0191431ba6a5dc74d2f9fe5657c14996
ssskram <paul.marks@pittsburghpa.gov>
2018-03-05 23:50:54 -0500

    work order parameter

---
+++ Controllers/New.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DPW_maintenancerequest.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DPW_maintenancerequest.Controllers
+{
+    [Authorize]
+    public class New : Controller
+    {
+
+        public IActionResult WorkOrder(string OID)
+        {
+            ViewBag.OID = OID;
+            return View();
+        }
+        
+    }
+}
