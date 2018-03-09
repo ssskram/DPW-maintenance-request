370a0c79b9d891778e1d480f038bffa6d0c98189
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2018-03-09 13:35:28 -0500

    Nifty loader guy

--- DPW-facilities/app.R
+++ DPW-facilities/app.R
@@ -64,14 +64,46 @@ ui <- fluidPage(
   # Favicon
   tags$head(tags$link(rel = "shortcut icon", type = "image/png", href = "favicon.png")),
   # Big Map
-  tags$style(type = "text/css", '#map {height: calc(100vh - 77px) !important;}
+  tags$style(type = "text/css", 
+              '#map {
+                 height: calc(100vh - 77px) !important;
+                 z-index: 1035;
+              }
+              .overlayloader {
+                 align-items: center;
+                 justify-content: center;
+                 background-color: rgba(0,0,0,0.7);
+                 position: fixed;
+                 z-index: -1;
+                 height: 100%;
+                 width: 100%;
+                 top: 0;
+                 left: 0;
+                 overflow: hidden;
+             }
+             .loader {
+               position: absolute;
+               margin: auto;
+               top: 0;
+               right: 0;
+               bottom: 0;
+               left: 0;
+               width: 100px;
+               height: 100px;
+               border: 16px solid #fffaee;
+               border-top: 16px solid rgba(202, 205, 206,.35);
+               border-radius: 50%;
+               width: 150px;
+               height: 150px;
+               opacity: 0.7;
+               animation: spin 4s linear infinite;
+             }
+             @keyframes spin {
+               0% { transform: rotate(0deg); }
+               100% { transform: rotate(360deg); }
+             }
              .container-fluid {
-                padding:0; 
-                background-image: url("Loading_2.gif");
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-position: center; 
-                background-size: 198px 198px; 
+                padding:0;
              }
              .shiny-input-panel {
                 background-color: #fffaee;
@@ -103,10 +135,12 @@ ui <- fluidPage(
   # Run base URL
   tags$script(baseUrl),
   inputPanel(style = "padding-left: 42px; padding-right: 0px; padding-top: 0px; overflow-y: visible; background-color: #fffaee;",
-                uiOutput("search_field")#,
-             # HTML('<div style = "position: absolute; right: 24px; top: 13px;">Click on a facility then click the "Submit a Maintenance Request" link!<br></div>')
+                uiOutput("search_field")
              ),
-  leafletOutput("map")
+  leafletOutput("map"),
+  HTML('<div class="overlayloader">
+          <div class="loader"></div>
+       </div>')
   )
 
 # Define server logic required to draw a histogram
