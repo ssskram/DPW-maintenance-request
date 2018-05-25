2833f3e4ad22fe5a6b61f1dcef15f2680668e95b
ssskram <paul.marks@pittsburghpa.gov>
2018-05-25 16:24:40 -0400

    fixed buttons & readme

--- ClientApp/components/Table.tsx
+++ ClientApp/components/Table.tsx
@@ -51,7 +51,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget posuere sapien. Phasellus blandit neque porttitor.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -61,7 +61,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -71,7 +71,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim massa elit, sit amet vestibulum libero.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -81,7 +81,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -93,7 +93,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat porta varius. Donec nulla nisl, tincidunt vel consequat eu, varius eu augue. Quisque iaculis blandit est sed suscipit.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -103,7 +103,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in pulvinar ipsum.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -114,7 +114,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Donec eget posuere sapien. Phasellus blandit neque porttitor.
                 </p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -124,7 +124,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit suscipit urna a ornare. Donec egestas at felis consequat rhoncus.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -136,7 +136,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -146,7 +146,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac ipsum a est laoreet laoreet. Sed augue nisl.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -156,7 +156,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget posuere sapien. Phasellus blandit neque porttitor.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
@@ -166,7 +166,7 @@ export default class selectTable extends React.Component<RouteComponentProps<{}>
                 <p>Address</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus porttitor elit, ac feugiat enim. Vivamus nec elit ultricies, volutpat velit at.</p>
                 <div className="text-center">
-                  <NavLink to="/" role="button" className="btn btn-default">Select</NavLink>
+                  <NavLink to="/Table" role="button" className="btn btn-default">Select</NavLink>
                 </div>
               </div>
             </div>
