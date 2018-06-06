6016b30fce94e422d3d3d522f5804f7aa6d23e36
ssskram <paul.marks@pittsburghpa.gov>
2018-06-06 15:51:45 -0400

    message store

--- ClientApp/store/messages.ts
+++ ClientApp/store/messages.ts
@@ -23,7 +23,7 @@ export const actionCreators = {
 export const reducer: Reducer<MessageState> = (state: MessageState, action: KnownAction) => {
     switch (action.type) {
         case 'FORM_SUCCESS':
-            return { messages: "Success! Help is on it's way" };
+            return { messages: "Success! We'll be seeing you soon." };
         case 'FORM_ERROR':
             return { messages: "Oops! Something isn't right. Please try that again. If the problem persists, please contact I&P" };
         case 'ALREADY_SUBMITTED':
@@ -33,5 +33,5 @@ export const reducer: Reducer<MessageState> = (state: MessageState, action: Know
         default:
             const exhaustiveCheck: never = action;
     }
-    return state || { messages: "Success! Continue to monitor this site for progress on your request" }
+    return state || { messages: "" }
 };
