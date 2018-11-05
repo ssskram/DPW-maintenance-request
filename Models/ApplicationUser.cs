c247776e7ddc59e3994ab9b419e529c67afdf5cf
ssskram <paul.marks@pittsburghpa.gov>
2018-11-05 15:18:55 -0500

    clear out node branch

--- Models/ApplicationUser.cs
+++
@@ -1,5 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace maintenance_reqsts.Models {
-    public class ApplicationUser : IdentityUser { }
-}
--- Models/Issues.cs
+++
@@ -1,6 +0,0 @@
-namespace maintenance_reqsts.Models {
-    public class Issue {
-        public string type { get; set; }
-        public string name { get; set; }
-    }
-}
