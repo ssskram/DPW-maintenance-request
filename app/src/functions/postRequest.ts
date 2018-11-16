e6f76ab2c8fc8e37da39274b70bd83fa661130b6
ssskram <paul.marks@pittsburghpa.gov>
2018-11-16 12:33:27 -0500

    error handling for post/img post

--- app/src/functions/postRequest.ts
+++ app/src/functions/postRequest.ts
@@ -2,77 +2,58 @@
 
 export default async function postRequest(request, image, user) {
 
-    let data = {
-        Oid: 784077363
-    }
-    var requestSuccess = true
+    var postSuccess
     var Oid
 
-    if (data.Oid) {
-        requestSuccess = true
-        Oid = data.Oid
-    } else {
-        requestSuccess = false
+    // format data
+    let data = JSON.stringify({
+        ActivityField: "Investigate",
+        DepartmentField: "Facilities",
+        cgAssetTypeField: "Facility",
+        cgAssetAndIdField: "Facility " + request.building,
+        StatusField: "Planned",
+        cgAssetIDField: request.building,
+        RequestIssueField: request.issue,
+        TaskDescriptionField: request.description,
+        RequestDepartmentField: request.department,
+        LocationDescriptionField: request.location,
+        RequestorPhoneNumberField: request.phone,
+        RequesterEmailField: user
+    })
+    let cleanedData = data.replace(/'/g, '')
+    const body = '{ "cgTasksClass" : [ ' + cleanedData + ' ] }'
+
+    // await post response
+    const dataResponse = await fetch('https://cartegraphapi.azurewebsites.us/maintenanceRequests/newRequest', {
+        method: 'POST',
+        body: body,
+        headers: new Headers({
+            'Authorization': 'Bearer ' + process.env.REACT_APP_CART_API,
+            'Content-Type': 'application/json'
+        })
+    })
+
+    try {
+        const dataJson = await dataResponse.json()
+        Oid = dataJson.Oid
+        postSuccess = true
+    } catch {
+        postSuccess = false
     }
 
-    console.log(requestSuccess)
-    console.log(Oid)
-
-    // // first, post request
-    // let data = JSON.stringify({
-    //     ActivityField: "Investigate",
-    //     DepartmentField: "Facilities",
-    //     cgAssetTypeField: "Facility",
-    //     cgAssetAndIdField: "Facility " + request.building,
-    //     StatusField: "Planned",
-    //     cgAssetIDField: request.building,
-    //     RequestIssueField: request.issue,
-    //     TaskDescriptionField: request.description,
-    //     RequestDepartmentField: request.department,
-    //     LocationDescriptionField: request.location,
-    //     RequestorPhoneNumberField: request.phone,
-    //     RequesterEmailField: user
-    // })
-    // let cleanedData = data.replace(/'/g, '')
-    // const body = '{ "cgTasksClass" : [ ' + cleanedData + ' ] }'
-    // fetch('https://cartegraphapi.azurewebsites.us/maintenanceRequests/newRequest', {
-    //     method: 'POST',
-    //     body: body,
-    //     headers: new Headers({
-    //         'Authorization': 'Bearer ' + process.env.REACT_APP_CART_API,
-    //         'Content-Type': 'application/json'
-    //     })
-    // })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         // TODO
-    //         // can't get data.Oid to return ture
-    //         // therefore, post returns as an error
-    //         // and can't proceed to image post
-    //         console.log(typeof Oid == 'number')
-    //         if (data.Oid) {
-    //             requestSuccess = true
-    //             Oid = data.Oid
-    //         } else {
-    //             requestSuccess = false
-    //         }
-    //     })
-
-    // then, post all of the images
-    if (requestSuccess == true) {
+    // if post succeeded, post the image
+    if (postSuccess == true) {
         if (image.length > 0) {
             const cleanedName = image[0].name.replace(/[,"+/()'\s]/g, '')
-            const response = await fetch('http://localhost:3000/maintenanceRequests/addImage?oid=' + Oid + '&filename=' + cleanedName, {
+            await fetch('https://cartegraphapi.azurewebsites.us/maintenanceRequests/addImage?oid=' + Oid + '&filename=' + cleanedName, {
                 method: 'POST',
                 body: image,
                 headers: new Headers({
                     'Authorization': 'Bearer ' + process.env.REACT_APP_CART_API
                 })
             })
-            const json = await response.json()
-            console.log(json)
         }
     }
 
-    return requestSuccess
+    return postSuccess
 }
