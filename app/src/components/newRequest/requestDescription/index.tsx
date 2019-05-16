1d8b5111ddc0e82871aee181598a3a7b6846f36f
ssskram <paul.marks@pittsburghpa.gov>
2019-05-16 11:31:41 -0400

    scafolding

---
+++ app/src/components/newRequest/requestDescription/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as types from "../../../store/types";
+
+type props = {};
+
+export default class RequestDescription extends React.Component<props, {}> {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return <div />;
+  }
+}
---
+++ app/src/components/newRequest/requestType/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as types from "../../../store/types";
+
+type props = {};
+
+export default class RequestType extends React.Component<props, {}> {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
