0abd8f3a304eed36a8241cebb656681b9879674f
ssskram <paul.marks@pittsburghpa.gov>
2019-05-16 10:40:12 -0400

    migrated store

--- app/src/store/constants/index.ts
+++
@@ -1,9 +0,0 @@
-
-// user
-export const loadUser = 'loadUser'
-
-// messages
-export const newMessage = 'reservationSuccess'
-export const success = 'successMessage'
-export const failure = 'failureMessage'
-export const clear = 'clearMessage'
